9ab9d49e7e4ffdf9bbde2c70fc31621dbe7491e8
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-09-04 20:14:33 +0700

    30 menit

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let angka = [8,3,9,2,5]
 
 function cariAngka (){
     let nilaiAngka = angka;
+
+    for (nilaiAngka; nilaiAngka === 3; nilaiAngka++){
+        console.log(nilaiAngka);
+    }
+
+
     if (angka === 3){
         return(angka);
     } else if (angka !== 3){
